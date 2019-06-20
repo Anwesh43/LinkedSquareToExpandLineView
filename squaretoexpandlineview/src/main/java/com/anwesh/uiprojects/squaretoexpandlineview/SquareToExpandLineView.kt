5fc48ffd95d6c166f88d53baedaebe9c05260c2d
Anwesh43 <anweshthecool0@gmail.com>
2019-06-20 11:12:35 +0530

    adding Int and Float extension methods

--- squaretoexpandlineview/src/main/java/com/anwesh/uiprojects/squaretoexpandlineview/SquareToExpandLineView.kt
+++ squaretoexpandlineview/src/main/java/com/anwesh/uiprojects/squaretoexpandlineview/SquareToExpandLineView.kt
@@ -20,3 +20,13 @@ val scGap : Float = 0.05f
 val scDiv : Double = 0.51
 val foreColor : Int = Color.parseColor("#01579B")
 val backColor : Int = Color.parseColor("#BDBDBD")
+
+fun Int.inverse() : Float = 1f / this
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.mirrorValue(a : Int, b : Int) : Float {
+    val k : Float = scaleFactor()
+    return (1 - k) * a.inverse() + k * b.inverse()
+}
+fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
