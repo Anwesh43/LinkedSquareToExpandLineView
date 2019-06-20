c2e1185d43a3f3483ead176aeacb993acfa2579d
Anwesh43 <anweshthecool0@gmail.com>
2019-06-20 11:08:47 +0530

    adding libraries and constants

--- squaretoexpandlineview/src/main/java/com/anwesh/uiprojects/squaretoexpandlineview/SquareToExpandLineView.kt
+++ squaretoexpandlineview/src/main/java/com/anwesh/uiprojects/squaretoexpandlineview/SquareToExpandLineView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.squaretoexpandlineview
 /**
  * Created by anweshmishra on 20/06/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 4
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val foreColor : Int = Color.parseColor("#01579B")
+val backColor : Int = Color.parseColor("#BDBDBD")
